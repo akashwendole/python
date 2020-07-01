4e2ddb81d6f94ea7061277da78b5a6998e0a2d18
akash <akash.wendole3020@gmail.com>
2020-07-01 23:34:58 +0530

    second day ended

---
+++ basics of python.py
@@ -0,0 +1,15 @@
+print('Hello world!')
+print("Hello" + " world!")  #string concatination
+
+
+
+print('\n\n\nMy First Program')
+number_one = int(input("Enter the first number: "))
+number_two = int(input("Enter the second number: "))
+print("Calculating the result....")
+result = number_one + number_two
+print("The result is: " + str(result))
+
+
+
+print()
